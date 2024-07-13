eac59988141b57c87496ddde38b4affc72fa1c3d
prathmesh <prathameshk990@gmail.com>
2024-07-13 21:03:11 +0530

    education css

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -6,7 +6,7 @@ import "../App.css";
 export const Education = () => {
   return (
     <section className=" bg-[#0a1019]">
-      <div className="w-10/12 max-w-maxContent mx-auto pt-16 md:pt-32 pb-14">
+      <div className="w-10/12 max-w-maxContent mx-auto pt-16 md:pt-32 pb-28">
         <div>
           <h3 className="text-2xl md:text-4xl  leading-4 text-[#55e6a5] font-sans  font-semibold uppercase mb-7 md:mb-10">
             Education
@@ -18,13 +18,13 @@ export const Education = () => {
                   <div className="w-28 p-2 md:w-48 md:h-16 border-[2px] border-[#55e6a5] rounded-sm text-[#55e6a5] text-lg md:text-2xl  font-sans font-medium  text-center flex items-center justify-center ">
                     {ele.passing}
                   </div>
-                  <h1 className="text-white text-[1.2rem] text-start md:text-start font-semibold md:font-bold md:text-2xl md:archivo-black-regular ">
+                  <h1 className="text-white text-[1.1rem] text-start md:text-start font-semibold md:font-bold md:text-2xl md:archivo-black-regular ">
                     {ele.school}
                   </h1>
                   <h1 className="text-xl text-white text-opacity-50 .poppins-extrabold  md:ml-[10px]">
                     {ele.place}
                   </h1>
-                  <h1 className="text-xl -mt-3  md:text-2xl font-medium text-white text-opacity-75">
+                  <h1 className="text-xl md:-mt-3  md:text-2xl font-medium text-white text-opacity-75">
                     {ele.deg}
                   </h1>
                 </div>
