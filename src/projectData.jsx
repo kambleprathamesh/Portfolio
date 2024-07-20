2aecdde5d3f432cdbdafbb3091cc43497c9b5eea
prathmesh <prathameshk990@gmail.com>
2024-07-20 20:09:34 +0530

    project links added

--- src/projectData.jsx
+++ src/projectData.jsx
@@ -3,12 +3,13 @@ import Amz from "./projectImages/Amazon.png"; //2
 import git from "./projectImages/GitHubUserApp.png"; //3
 import pass from "./projectImages/Password.png"; //5
 import parallex from "./projectImages/Parralex.png"; //7
-import ticTac from "./projectImages/ticTac.png"; //6
-import SPS from "./projectImages/SPS.png"; //9
-import currency from "./projectImages/Currency.png"; //8
+// import ticTac from "./projectImages/ticTac.png"; //6
+// import SPS from "./projectImages/SPS.png"; //9
+// import currency from "./projectImages/Currency.png"; //8
 import razorPay from "./projectImages/razorPay.png"; //1
-import calci from "./projectImages/Calci.png"; //10
+// import calci from "./projectImages/Calci.png"; //10
 import studynotion from "./images/studyNotion.png"
+import campus from "./projectImages/campus.png"
 const data = [
   {
     id: 1,
@@ -21,7 +22,7 @@ const data = [
   {
     id: 2,
     title: "Campus connect",
-    img: studynotion,
+    img: campus,
     code: "",
     viewLink: "",
     techStack: "MERN ",
@@ -30,82 +31,54 @@ const data = [
     id: 3,
     title: "RazorPay UI Clone",
     img: razorPay,
-    code: "",
-    viewLink: "",
+    code: "https://github.com/kambleprathamesh/razorpayUI",
+    viewLink: "https://razorpay-frontend-ui.netlify.app/",
     techStack: "ReactJs  TailwindCss",
   },
   {
     id: 4,
     title: "Amazon UI Clone",
     img: Amz,
-    code: "",
-    view: "",
+    code: "https://github.com/kambleprathamesh/Amazon-Clone",
+    view: "https://kambleprathamesh.github.io/Amazon-Clone/",
     techStack: "Html Css",
   },
   {
     id: 5,
     title: "Git-Hub User App",
     img: git,
-    code: "",
-    viewLink: "",
+    code: "https://github.com/kambleprathamesh/GitHub-Profile-App",
+    viewLink: "https://kambleprathamesh.github.io/GitHub-Profile-App/",
     techStack: "Html Css JavaScript",
   },
   {
     id: 6,
     title: "Weather Application",
     img: weatherApp,
-    code: "",
-    viewLink: "",
+    code: "https://github.com/kambleprathamesh/weather-App",
+    viewLink: " https://kambleprathamesh.github.io/weather-App/",
     techStack: "Html Css Js",
   },
   {
     id: 7,
     title: "Password Generator",
     img: pass,
-    code: "",
-    viewLink: "",
+    code: "https://github.com/kambleprathamesh/Password-generator",
+    viewLink: "https://kambleprathamesh.github.io/Password-generator/",
     techStack: "Html Css Js",
   },
-  {
-    id: 8,
-    title: "Rock Paper Scicors",
-    img: SPS,
-    code: "",
-    viewLink: "",
-    techStack: "Html Css Js",
-  },
+  
   {
     id: 9,
     title: "Parallex Website",
     img: parallex,
-    code: "",
-    viewLink: "",
+    code: "https://github.com/kambleprathamesh/Parallax-WebPage",
+    viewLink: "https://kambleprathamesh.github.io/Parallax-WebPage/",
     techStack: "Html Css Js",
   },
-  {
-    id: 10,
-    title: "Currency Converter App",
-    img: currency,
-    code: "",
-    viewLink: "",
-    techStack: "Html Css Js",
-  },
-  {
-    id: 11,
-    title: "Tic Tac Toe",
-    img: ticTac,
-    code: "",
-    viewLink: "",
-    techStack: "Html Css Js",
-  },
-  {
-    id: 12,
-    title: "Calculator App",
-    img: calci,
-    code: "",
-    viewLink: "",
-    techStack: "Html Css Js",
-  },
+  
+  
+  
 ];
 
 
