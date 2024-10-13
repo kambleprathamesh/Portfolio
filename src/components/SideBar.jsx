518bba17a49557e0cd763296c04a86714dc3949a
prathmesh <prathameshk990@gmail.com>
2024-10-13 12:00:28 +0530

    Home

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,7 +8,14 @@ const sideBar = () => {
         <div className="w-11/12 h-1/2  max-w-maxContent ">
           {sidemenu.map((ele, index) => {
             return (
-              <ul className="w-[90%] flex flex-col items-start justify-start space-y-2 mt-2">
+              <ul
+                className="w-[90%] flex flex-col items-start justify-start space-y-2 mt-2"
+                style={{
+                  background: "linear-gradient(to right, #6dd5ed, #80e3aa)",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent", // For Safari support
+                }}
+              >
                 <a href={ele.id} key={index}>
                   <li className="w-full text-lg text-start text-[#55e6a5] ">
                     {ele.menu}
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,7 +10,7 @@ export const Home = () => {
     <section className="w-full h-screen md:h-screen pb-10">
       <div className=" w-[85%] mx-auto flex flex-col  md:flex-row justify-center">
         <div className="w-full flex flex-col justify-start gap-y-8  pt-[2rem] md:pt-[8rem]">
-          <h1 className="text-white  text-3xl lg:text-5xl  uppercase font-Rowdies font-sans font-semibold pb-10  md:pb-0">
+          <h1 className="text-white  text-3xl font-bold lg:text-5xl  uppercase font-Rowdies font-sans  pb-10  md:pb-0">
             Hi,I'm Prathmesh! <br></br>
             <div class="dropping-texts  text-[#55e6a5]  text-3xl font-sans  lg:text-6xl">
               <div>Coder</div>
